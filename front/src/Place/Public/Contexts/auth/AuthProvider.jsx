e0b6c22df9ae819573628380c025a69a9b28ade6
Phattran0604 <tranphat701@gmail.com>
2023-05-22 17:04:30 +0700

    update version cart

--- front/src/Place/Public/Contexts/auth/AuthProvider.jsx
+++ front/src/Place/Public/Contexts/auth/AuthProvider.jsx
@@ -2,125 +2,113 @@
 import { useEffect, useReducer } from "react";
 
 import AuthContext from "./auth-context";
-import axios from "axios";
 
 const initialState = {
-  user: null,
-  name: null,
-  lastName: null,
-  email: null,
-  phoneNumber: null,
-  addresses: [],
-  isVerified: false,
-  authIsReady: false,
-  role_as:0
+    user: null,
+    name: null,
+    lastName: null,
+    email: null,
+    phoneNumber: null,
+    addresses: [],
+    isVerified: false,
+    authIsReady: false,
+    role_as: 0,
 };
 
 const authReducer = (state, action) => {
-  switch (action.type) {
-    case "AUTH_IS_READY": {
-      return {
-        user: action.payload.user,
-        name: action.payload.name,
-        lastName: action.payload.lastName,
-        email: action.payload.email,
-        phoneNumber: action.payload.phoneNumber || null,
-        addresses: action.payload.addresses || [],
-        isVerified: true,
-        authIsReady: true,
-        role_as:action.payload.role_as
-      };
-    }
+    switch (action.type) {
+        case "AUTH_IS_READY": {
+            return {
+                user: action.payload.user,
+                name: action.payload.name,
+                lastName: action.payload.lastName,
+                email: action.payload.email,
+                phoneNumber: action.payload.phoneNumber || null,
+                addresses: action.payload.addresses || [],
+                isVerified: true,
+                authIsReady: true,
+                role_as: action.payload.role_as,
+            };
+        }
 
-    case "ANONYMOUS_AUTH_IS_READY": {
-      return {
-        ...initialState,
-        user: action.payload.user,
-        authIsReady: true,
-      };
-    }
+        case "ANONYMOUS_AUTH_IS_READY": {
+            return {
+                ...initialState,
+                user: action.payload.user,
+                authIsReady: true,
+            };
+        }
 
-    case "LOGIN": {
-      return {
-        ...state,
-        user: action.payload.user,
-        name: action.payload.name,
-        lastName: action.payload.lastName,
-        email: action.payload.email,
-        phoneNumber: action.payload.phoneNumber || null,
-        addresses: action.payload.addresses || [],
-        isVerified: action.payload.isVerified,
-        role_as:action.payload.role_as
-      };
-    }
+        case "LOGIN": {
+            return {
+                ...state,
+                user: action.payload.user,
+                name: action.payload.name,
+                lastName: action.payload.lastName,
+                email: action.payload.email,
+                phoneNumber: action.payload.phoneNumber || null,
+                addresses: action.payload.addresses || [],
+                isVerified: action.payload.isVerified,
+                role_as: action.payload.role_as,
+            };
+        }
 
-    case "LOGOUT": {
-      return {
-        ...initialState,
-      };
-    }
+        case "LOGOUT": {
+            return {
+                ...initialState,
+            };
+        }
 
-    case "UPDATE_USER": {
-      return {
-        ...state,
-        ...action.payload,
-      };
-    }
+        case "UPDATE_USER": {
+            return {
+                ...state,
+                ...action.payload,
+            };
+        }
 
-    case "UPDATE_ADDRESSES": {
-      return {
-        ...state,
-        addresses: action.payload,
-      };
-    }
+        case "UPDATE_ADDRESSES": {
+            return {
+                ...state,
+                addresses: action.payload,
+            };
+        }
 
-    default: {
-      return state;
+        default: {
+            return state;
+        }
     }
-  }
 };
 
 const AuthProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(authReducer, initialState);
-  useEffect(() => {
-    const check = (user) => {
-      
-      axios.get(`api/currentLogin`).then((res) => {
-        if (res.data.status === 200) {
-          if (res.data.user) {
-            const dataUser = res.data.user;
-            dispatch({
-              type: "AUTH_IS_READY",
-              payload: { user, ...dataUser },
-            });
-          } else {
-            const user = res.data.anonymous;
-            dispatch({
-              type: "ANONYMOUS_AUTH_IS_READY",
-              payload: { user },
-            });
-          }
+    const [state, dispatch] = useReducer(authReducer, initialState);
+    useEffect(() => {
+        const check = (user) => {
+            if (localStorage.getItem("auth_token")) {
+                const dataUser = localStorage.getItem("auth_token");
+                dispatch({
+                    type: "AUTH_IS_READY",
+                    payload: { user, ...dataUser },
+                });
+            } else {
+                dispatch({
+                    type: "ANONYMOUS_AUTH_IS_READY",
+                    payload: { user },
+                });
+            }
 
-          console.log(res);
-        } else if (res.data.status === 401) {
-          // swal("Warning",res.data.message,"warning");
-        } else {
-          // setLogin({...loginInput, error_list: res.data.validation_errors });
-        }
-      });
-      
-    };
-   
-    return () => check();
-  }, []);
+            // console.log(res);
+        };
 
-  console.log("auth-context", state);
+        return () => check();
+    }, []);
 
-  return (
-    <AuthContext.Provider value={{ ...state, dispatch }}>
-      {children}
-    </AuthContext.Provider>
-  );
+    console.log("auth-context", state);
+
+    return (
+        <AuthContext.Provider value={{ ...state, dispatch }}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
 
 export default AuthProvider;
--- front/src/Place/Public/Pages/ResetPassword/index.jsx
+++ front/src/Place/Public/Pages/ResetPassword/index.jsx
@@ -1,89 +1,123 @@
-import  { useEffect, useRef, useState } from 'react'
-import { Link, useLocation } from 'react-router-dom';
+import { useEffect, useRef, useState } from "react";
 import classNames from "classnames/bind";
 const cx = classNames.bind(style);
-import style from './index.module.scss';
-import { useLogin } from '../../Hooks/useLogin';
-import Toast from '../../Components/Toast';
-import ToastMessage from '../../Components/ToastMessage';
-import Loader from '../../Components/Loader';
-export default function ResetPassword() {
-    const { state: routerState } = useLocation();
+import style from "./index.module.scss";
+import { useLogin } from "../../Hooks/useLogin";
+import Toast from "../../Components/Toast";
+import ToastMessage from "../../Components/ToastMessage";
+import Loader from "../../Components/Loader";
+import queryString from "query-string";
+import axios from "axios";
 
-    // const { login, isLoading, error } = useLogin();
-  
-    const [toastMessage, setToastMessage] = useState(null);
-  
-    const emailInput = useRef();
-    const passwordInput = useRef();
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-  
-      await login({
-        email: emailInput.current.value,
-        password: passwordInput.current.value,
-      });
-    };
-  
+export default function ResetPassword() {
+    // const { state: routerState } = useLocation();
+    const [tokenMail, SetTokenMail] = useState("");
+    const { login, isLoading, error } = useLogin();
     useEffect(() => {
-      if (error) {
-        setToastMessage({ error, details: error.details });
-      }
-      // console.log(error);
-    }, [error]);
-  
-    const toggleToast = () => {
-      setToastMessage(null);
+        const url = window.location.href;
+        const parsed = queryString.parseUrl(url);
+        const token = parsed.query.token;
+
+        // Thực hiện xử lý với token ở đây
+        console.log(token);
+        SetTokenMail(token);
+    }, []);
+    const [toastMessage, setToastMessage] = useState(null);
+
+    const passwordInput = useRef();
+    const emailInput = useRef();
+    const confirmPasswordInput = useRef();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = {
+            email: emailInput.current.value,
+            password: passwordInput.current.value,
+            password_confirmation: confirmPasswordInput.current.value,
+            token: tokenMail,
+        };
+        axios.post("/api/mailResetPassword", data).then((res) => {
+            console.log(res.data.message);
+        });
+           await login({
+            email: emailInput.current.value,
+            password: passwordInput.current.value,
+        });
     };
-  
+
+    useEffect(() => {
+        if (error) {
+            setToastMessage({ error, details: error.details });
+        }
+        console.log(error);
+    }, [error]);
+
+    const toggleToast = () => {
+        setToastMessage(null);
+    };
+
     return (
-      <>
-        <Toast>
-          {toastMessage && (
-            <ToastMessage toggleToast={toggleToast} content={toastMessage} />
-          )}
-        </Toast>
-        {isLoading && <Loader />}
-        {!isLoading && (
-          <>
-            <section className={cx("nav_section")}></section>
-            <section className={cx("section")}>
-              <div className={cx("container")}>
-                <div className={`${cx("wrapper")} main-container`}>
-                  <form onSubmit={handleSubmit} className={cx("form")}>
-                    <h2 className={cx("title")}>Reset Password</h2>
-                  
-                    <label className={cx("label")}>
-                      <span>Password:</span>
-                      <input
-                        className={cx("input")}
-                        type="password"
-                        placeholder="password"
-                        required
-                        ref={passwordInput}
-                      />
-                    </label>
-                    <label className={cx("label")}>
-                      <span>Confirm Password:</span>
-                      <input
-                        className={cx("input")}
-                        type="password"
-                        placeholder="confirm password"
-                        required
-                        ref={confirmPasswordInput}
-                      />
-                    </label>
-                    <button className={cx("button")} type="submit">
-                     Go To Website -&gt;
-                    </button>
-                  </form>
-                  
-                </div>
-              </div>
-            </section>
-          </>
-        )}
-      </>
+        <>
+            <Toast>
+                {toastMessage && (
+                    <ToastMessage
+                        toggleToast={toggleToast}
+                        content={toastMessage}
+                    />
+                )}
+            </Toast>
+            {isLoading && <Loader />}
+            {!isLoading && (
+            <>
+                <section className={cx("nav_section")}></section>
+                <section className={cx("section")}>
+                    <div className={cx("container")}>
+                        <div className={`${cx("wrapper")} main-container`}>
+                            <form
+                                onSubmit={handleSubmit}
+                                className={cx("form")}
+                            >
+                                <h2 className={cx("title")}>Reset Password</h2>
+
+                                <label className={cx("label")}>
+                                    <span>Email:</span>
+                                    <input
+                                        className={cx("input")}
+                                        type="email"
+                                        placeholder="email"
+                                        required
+                                        ref={emailInput}
+                                    />
+                                </label>
+                                <label className={cx("label")}>
+                                    <span>Password:</span>
+                                    <input
+                                        className={cx("input")}
+                                        type="password"
+                                        placeholder="password"
+                                        required
+                                        ref={passwordInput}
+                                    />
+                                </label>
+                                <label className={cx("label")}>
+                                    <span>Confirm Password:</span>
+                                    <input
+                                        className={cx("input")}
+                                        type="password"
+                                        placeholder="confirm password"
+                                        required
+                                        ref={confirmPasswordInput}
+                                    />
+                                </label>
+                                <button className={cx("button")} type="submit">
+                                    Go To Website -&gt;
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </section>
+            </>
+            )}
+        </>
     );
 }
