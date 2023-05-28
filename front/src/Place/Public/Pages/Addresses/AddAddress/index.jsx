0ec1dda6194d6ac7050d4c4b27d0d460de41973b
Phattran0604 <tranphat701@gmail.com>
2023-05-28 08:54:51 +0700

    Public address

--- front/src/Place/Public/Pages/Addresses/AddAddress/index.jsx
+++ front/src/Place/Public/Pages/Addresses/AddAddress/index.jsx
@@ -15,159 +15,169 @@ import ToastMessage from "../../../Components/ToastMessage";
 import Loader from "../../../Components/Loader";
 
 const AddAddress = ({ toggleAddAddressModal }) => {
-  const { createAddress, isLoading, error } = useAddress();
-  // const { createAddress, isLoading, error } = [];
+    const { createAddress, isLoading, error } = useAddress();
+    // const { createAddress, isLoading, error } = [];
 
-  const [isChecked, setIsChecked] = useState(false);
-  const [notification, setNotification] = useState(false);
-  const [toastMessage, setToastMessage] = useState(null);
+    const [isChecked, setIsChecked] = useState(false);
+    const [notification, setNotification] = useState(false);
+    const [toastMessage, setToastMessage] = useState(null);
 
-  const handleCheckboxInput = () => {
-    setIsChecked((prevState) => !prevState);
-  };
+    const handleCheckboxInput = () => {
+        setIsChecked((prevState) => !prevState);
+    };
 
-  const nameInput = useRef();
-  const lastNameInput = useRef();
-  const phoneNumberInput = useRef();
-  const addressInput = useRef();
-  const zipCodeInput = useRef();
-  const cityInput = useRef();
-  const provinceInput = useRef();
+    const nameInput = useRef();
+    const lastNameInput = useRef();
+    const phoneNumberInput = useRef();
+    const addressInput = useRef();
+    const zipCodeInput = useRef();
+    const cityInput = useRef();
+    const provinceInput = useRef();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    await createAddress({
-      name: nameInput.current.value,
-      lastName: lastNameInput.current.value,
-      phoneNumber: phoneNumberInput.current.value,
-      address: addressInput.current.value,
-      zipCode: zipCodeInput.current.value,
-      city: cityInput.current.value,
-      province: provinceInput.current.value,
-      isMain: isChecked,
-    });
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await createAddress({
+            name: nameInput.current.value,
+            lastName: lastNameInput.current.value,
+            phoneNumber: phoneNumberInput.current.value,
+            address: addressInput.current.value,
+            zipCode: zipCodeInput.current.value,
+            city: cityInput.current.value,
+            province: provinceInput.current.value,
+            isMain: isChecked,
+        });
 
-    setNotification(true);
-  };
+        setNotification(true);
+    };
 
-  useEffect(() => {
-    if (notification) {
-      if (error) {
-        setToastMessage({ error, details: error.details });
-        setNotification(false);
-      } else {
+    useEffect(() => {
+        if (notification) {
+            if (error) {
+                setToastMessage({ error, details: error.details });
+                setNotification(false);
+            } else {
+                toggleAddAddressModal();
+            }
+        }
+    }, [notification]);
+
+    const toggleToast = () => {
+        setToastMessage(null);
+    };
+
+    useKeyDown(() => {
         toggleAddAddressModal();
-      }
-    }
-  }, [notification]);
+    }, ["Escape"]);
 
-  const toggleToast = () => {
-    setToastMessage(null);
-  };
+    return (
+        <>
+            <Toast>
+                {toastMessage && (
+                    <ToastMessage
+                        toggleToast={toggleToast}
+                        content={toastMessage}
+                    />
+                )}
+            </Toast>
+            {isLoading && (
+                <Loader
+                    noPortal={true}
+                    wrapperClassName={styles.loader_wrapper}
+                />
+            )}
+            {!isLoading && (
+                <form id="form" className={styles.form} onSubmit={handleSubmit}>
+                    <h2 className={styles.title}>Add Address:</h2>
+                    <div className={styles.form_inputs_wrapper}>
+                        <label className={styles.label}>
+                            <span>Name:</span>
+                            <input
+                                className={styles.input}
+                                type="text"
+                                placeholder="Name"
+                                required
+                                ref={nameInput}
+                            />
+                        </label>
+                        <label className={styles.label}>
+                            <span>Last name:</span>
+                            <input
+                                className={styles.input}
+                                type="text"
+                                placeholder="Last name"
+                                required
+                                ref={lastNameInput}
+                            />
+                        </label>
+                        <label className={styles.label}>
+                            <span>Phone:</span>
+                            <input
+                                className={styles.input}
+                                type="tel"
+                                required
+                                ref={phoneNumberInput}
+                            />
+                        </label>
+                        <label className={styles.label}>
+                            <span>Address:</span>
+                            <input
+                                className={styles.input}
+                                type="text"
+                                required
+                                ref={addressInput}
+                            />
+                        </label>
+                        <label className={styles.label}>
+                            <span>City/Town:</span>
+                            <input
+                                className={styles.input}
+                                type="text"
+                                required
+                                ref={cityInput}
+                            />
+                        </label>
+                        <label className={styles.label}>
+                            <span>Postal Code:</span>
+                            <input
+                                className={styles.input}
+                                type="text"
+                                inputMode="nuAddAddressModalmeric"
+                                required
+                                ref={zipCodeInput}
+                            />
+                        </label>
 
-  useKeyDown(() => {
-    toggleAddAddressModal();
-  }, ['Escape']);
-
-  return (
-    <>
-      <Toast>
-        {toastMessage && (
-          <ToastMessage toggleToast={toggleToast} content={toastMessage} />
-        )}
-      </Toast>
-      {isLoading && (
-        <Loader noPortal={true} wrapperClassName={styles.loader_wrapper} />
-      )}
-      {!isLoading && (
-        <form id="form" className={styles.form} onSubmit={handleSubmit}>
-          <h2 className={styles.title}>Add Address:</h2>
-          <div className={styles.form_inputs_wrapper}>
-            <label className={styles.label}>
-              <span>Name:</span>
-              <input
-                className={styles.input}
-                type="text"
-                placeholder="Name"
-                required
-                ref={nameInput}
-              />
-            </label>
-            <label className={styles.label}>
-              <span>Last name:</span>
-              <input
-                className={styles.input}
-                type="text"
-                placeholder="Last name"
-                required
-                ref={lastNameInput}
-              />
-            </label>
-            <label className={styles.label}>
-              <span>Phone:</span>
-              <input
-                className={styles.input}
-                type="tel"
-                required
-                ref={phoneNumberInput}
-              />
-            </label>
-            <label className={styles.label}>
-              <span>Address:</span>
-              <input
-                className={styles.input}
-                type="text"
-                required
-                ref={addressInput}
-              />
-            </label>
-            <label className={styles.label}>
-              <span>City/Town:</span>
-              <input
-                className={styles.input}
-                type="text"
-                required
-                ref={cityInput}
-              />
-            </label>
-            <label className={styles.label}>
-              <span>Postal Code:</span>
-              <input
-                className={styles.input}
-                type="text"
-                inputMode="nuAddAddressModalmeric"
-                required
-                ref={zipCodeInput}
-              />
-            </label>
-
-            <label className={styles.label}>
-              <span>Province</span>
-              <input
-                className={styles.input}
-                type="text"
-                required
-                ref={provinceInput}
-              />
-            </label>
-            <label className={styles.checkbox}>
-              <input
-                className={styles.input}
-                type="checkbox"
-                onChange={handleCheckboxInput}
-              />
-              <div>Default address</div>
-            </label>
-          </div>
-          <div className={styles.button_wrapper}>
-            <button form="form" className={styles.button} type="submit">
-              Add
-            </button>
-          </div>
-        </form>
-      )}
-    </>
-  );
+                        <label className={styles.label}>
+                            <span>Province</span>
+                            <input
+                                className={styles.input}
+                                type="text"
+                                required
+                                ref={provinceInput}
+                            />
+                        </label>
+                        <label className={styles.checkbox}>
+                            <input
+                                className={styles.input}
+                                type="checkbox"
+                                onChange={handleCheckboxInput}
+                            />
+                            <div>Default address</div>
+                        </label>
+                    </div>
+                    <div className={styles.button_wrapper}>
+                        <button
+                            form="form"
+                            className={styles.button}
+                            type="submit"
+                        >
+                            Add
+                        </button>
+                    </div>
+                </form>
+            )}
+        </>
+    );
 };
 
 export default AddAddress;
